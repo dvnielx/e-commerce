493222efa726e07a5bd9926b4243c74b1140340d
Daniela Castro <dgonzalezcs@outlook.com>
2023-06-10 19:35:46 -0600

    All routes added

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -9,10 +9,12 @@ import './App.css';
 
 const AppRoutes = () => {
   let routes = useRoutes([
-    {
-      path: '/',
-      element: <Home />
-    }
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount/> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
   ])
   return routes
 }
