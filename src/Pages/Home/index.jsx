d2e4968da3fb16d44742c456cdae87bf693f7bc8
Daniela Castro <dgonzalezcs@outlook.com>
2023-06-11 20:05:05 -0600

    Layout in Components added

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Layout from '../../Components/Layout';
 
 function Home() {
   return (
-    <div>Home</div>
+    <Layout>
+      Home
+    </Layout>
   )
 }
 
