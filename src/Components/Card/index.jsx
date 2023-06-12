76bb078fc9c1f4798c5373fd89aca7707aaa6838
Daniela Castro <dgonzalezcs@outlook.com>
2023-06-11 22:09:50 -0600

    API consumed and adding styles to the grid

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (data) => {
   return (
-    <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
+    <div className='bg-white cursor-pointer w-45 h-50 rounded-lg'>
         <figure className='relative mb-2 w-full h-4/5'> 
-            <span className='absolute bottom-0 eft-0 mg-2 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>Electronics</span>
-                <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/17131288/pexels-photo-17131288/free-photo-of-senderos-del-canon-del-antilope.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='headphones' />
+            <span className='absolute bottom-0 eft-0 mg-2 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
+                <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.description} />
                 <div className='absolute top-0 right-0 flex justify-center items-center m-2 bg-white w-6 h-6 rounded-full p-1'> 
                 + </div>
         </figure>
         <p className='flex justify-between'>
-            <span className='text-sm font-light'>Hadphones</span>
-            <span className='text-lg font-medium'>$300</span>
+            <span className='text-sm font-light'>{data.data.title}</span>
+            <span className='text-lg font-medium'>${data.data.price}</span>
         </p>
     </div>
   )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 import Layout from '../../Components/Layout';
 import Card from '../../Components/Card';
+import { data } from 'autoprefixer';
 
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then(response => (response.json())
+    .then(data => setItems(data)))
+  }, [])
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className='grid place-items-center gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-screen-xl'>
+      {
+        items?.map((item) => (
+         <Card key={item.id} data={item} />
+        ))
+      }
+      </div>
     </Layout>
   )
 }
