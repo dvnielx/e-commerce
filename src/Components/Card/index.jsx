32343d745f83edcddb72eb9c5543f581ddc88d81
Daniela Castro <dgonzalezcs@outlook.com>
2023-06-13 18:52:37 -0600

    Product Detail opens and closes

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -6,7 +6,8 @@ import { PlusIcon } from '@heroicons/react/24/solid';
 const Card = (data) => {
   const context = useContext(ShoppingCartContext)
   return (
-    <div className='bg-white cursor-pointer w-45 h-50 rounded-lg'>
+    <div className='bg-white cursor-pointer w-45 h-50 rounded-lg'
+    onClick={() => context.openProductDetail()}>
         <figure className='relative mb-2 w-full h-4/5'> 
             <span className='absolute bottom-0 eft-0 mg-2 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.description} />
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,13 +1,18 @@
+import { useContext } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/solid';
+import { ShoppingCartContext } from '../../Context';
 import './styles.css';
 
 const ProductDetails = () => {
+    const context = useContext(ShoppingCartContext)
+
     return(
-        <aside className='product-detail flex flex-col fixed right-2 border border-black rounded-lg bg-white'>
+        <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex flex-col fixed right-2 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center'>
-                <h2 className='font-medium text-xl p-4'>Detail</h2>
+                <h2 className='font-medium text-xl p-4'>Details</h2>
                 <div>
-                    <XMarkIcon className='h-5 w-5 text-black' />
+                    <XMarkIcon className='h-5 w-5 text-black cursor-pointer' 
+                    onClick={() => context.closeProductDetail()}/>
                 </div>
             </div>
         </aside>
